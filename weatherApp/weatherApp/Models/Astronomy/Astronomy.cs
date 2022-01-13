c62a348b73a5cf72479bc76234ed84b77703073a
glitchkn1ght <54705224+glitchkn1ght@users.noreply.github.com>
2022-01-13 13:57:32 +0000

    Added astronomy model

---
+++ weatherApp/weatherApp/Models/Astronomy/Astronomy.cs
@@ -0,0 +1,30 @@
+﻿//Change History
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// 12/01/2022 Ticket1 JS Team darkSaber - Initial version. 
+// --------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+namespace weatherApp.Models.Astronomy
+{
+    using Newtonsoft.Json;
+
+    public class Astronomy
+    {
+        [JsonProperty("sunrise")]
+        public string SunriseTime { get; set; }
+
+        [JsonProperty("sunrise")]
+        public string SunsetTime { get; set; }
+
+        [JsonProperty("moonrise")]
+        public string MoonRiseTime { get; set; }
+
+        [JsonProperty("moonset")]
+        public string MoonSetTime { get; set; }
+
+        [JsonProperty("moon_phase")]
+        public string MoonPhase { get; set; }
+
+        [JsonProperty("moon_illumination")]
+        public string MoonIllumination { get; set; }
+    }
+}
